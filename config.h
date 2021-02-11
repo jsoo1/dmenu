8bbe57738d5e6ab19db48c0620330c4c1dd7876d
John Soo <jsoo1@asu.edu>
2021-02-10 16:21:37 -0800

    Lower font size.

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"iosevka:size=14",
-	"monospace:size=14"
+	"iosevka:size=12",
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -19,7 +19,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 20;
-static unsigned int lineheight = 36;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 24;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
